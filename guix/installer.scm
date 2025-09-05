d5f8577790cf730c50d4fab5529fedc5be7d6ec0
Jon Hermansen <jon@jh86.org>
2025-09-05 07:30:41 -0400

    Cleanup was too aggressive. Cherry pick only networking firmwares

--- guix/installer.scm
+++ guix/installer.scm
@@ -61,7 +61,14 @@
   (operating-system
     (inherit installation-os)
     (kernel linux)
-    (firmware (list amdgpu-firmware))
+    (firmware (list
+	       ;; amdgpu-firmware
+	       atheros-firmware
+	       broadcom-sta
+	       ;; i915-firmware
+	       iwlwifi-firmware
+	       mediatek-firmware
+	       realtek-firmware))
 
     ;; Add the 'net.ifnames' argument to prevent network interfaces
     ;; from having really long names.  This can cause an issue with
